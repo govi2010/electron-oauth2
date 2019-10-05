9763a13e507022ae4355cd77ef5be83ef9d4603b
GOVINDA ALWANI <govi2010@gmail.com>
2019-10-05 14:41:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,155 +10,181 @@ const session = electron.session;
 const BrowserWindow = electron.BrowserWindow;
 
 var generateRandomString = function(length) {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var text = "";
+    var possible =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
 
-  return text;
+    return text;
 };
 
 module.exports = function(config, windowParams) {
-  function getAuthorizationCode(opts) {
-    opts = opts || {};
+    function getAuthorizationCode(opts, callback) {
+        opts = opts || {};
 
-    if (!config.redirectUri) {
-      config.redirectUri = "urn:ietf:wg:oauth:2.0:oob";
-    }
-
-    var urlParams = {
-      response_type: "code",
-      redirect_uri: config.redirectUri,
-      client_id: config.clientId,
-      state: generateRandomString(16)
-    };
-
-    if (opts.scope) {
-      urlParams.scope = opts.scope;
-    }
-
-    if (opts.accessType) {
-      urlParams.access_type = opts.accessType;
-    }
-
-    var url = config.authorizationUrl + "?" + queryString.stringify(urlParams);
-
-    return new Promise(function(resolve, reject) {
-      const authWindow = new BrowserWindow(windowParams || { "use-content-size": true });
-
-      authWindow.loadURL(url);
-      authWindow.show();
-
-      authWindow.on("closed", function() {
-        reject(new Error("window was closed by user"));
-      });
-
-      function onCallback(url) {
-        var url_parts = nodeUrl.parse(url, true);
-        var query = url_parts.query;
-        var code = query.code;
-        var error = query.error;
-
-        if (error !== undefined) {
-          reject(error);
-          authWindow.removeAllListeners("closed");
-          setImmediate(function() {
-            setTimeout(function() {
-              authWindow.webContents.session.clearStorageData({
-                storages: ["appcache", "cookies", "filesystem", "shadercache"],
-                quotas: ["persistent", "syncable"]
-              }, function() {
-                authWindow.close();
-                authWindow.destroy();
-              });
-            }, 100);
-          });
-        } else if (code) {
-          resolve(code);
-          authWindow.removeAllListeners("closed");
-          setImmediate(function() {
-            setTimeout(function() {
-              authWindow.webContents.session.clearStorageData({
-                storages: ["appcache", "cookies", "filesystem", "shadercache"],
-                quotas: ["persistent", "syncable"]
-              }, function() {
-                authWindow.close();
-                authWindow.destroy();
-              });
-            }, 100);
-          });
+        if (!config.redirectUri) {
+            config.redirectUri = "urn:ietf:wg:oauth:2.0:oob";
         }
-      }
 
-      authWindow.webContents.on("will-navigate", function(event, url) {
-        onCallback(url);
-      });
-      var filter = {
-        urls: [config.redirectUri + "*"]
-      };
-      authWindow.webContents.session.webRequest.onBeforeRequest(filter, (details, callback) => {
-        var url = details.url;
-        if (url.startsWith(config.redirectUri)) {
-          onCallback(url);
-        }
-        callback({});
-      });
-      authWindow.webContents.on("did-get-redirect-request", function(event, oldUrl, newUrl) {
-        onCallback(newUrl);
-      });
-    });
-  }
-
-  function tokenRequest(data) {
-    const header = {
-      "Accept": "application/json",
-      "Content-Type": "application/x-www-form-urlencoded"
-    };
-
-    if (config.useBasicAuthorizationHeader) {
-      header.Authorization = "Basic " + new Buffer(config.clientId + ":" + config.clientSecret).toString("base64");
-    } else {
-      objectAssign(data, {
-        client_id: config.clientId,
-        client_secret: config.clientSecret
-      });
-    }
-
-    return fetch(config.tokenUrl, {
-      method: "POST",
-      headers: header,
-      body: queryString.stringify(data)
-    }).then(function(res) {
-      return res.json();
-    });
-  }
-
-  function getAccessToken(opts) {
-    return getAuthorizationCode(opts)
-      .then(function(authorizationCode) {
-        var tokenRequestData = {
-          code: authorizationCode,
-          grant_type: "authorization_code",
-          redirect_uri: config.redirectUri
+        var urlParams = {
+            response_type: "code",
+            redirect_uri: config.redirectUri,
+            client_id: config.clientId,
+            state: generateRandomString(16)
         };
-        tokenRequestData = Object.assign(tokenRequestData, opts.additionalTokenRequestData);
-        return tokenRequest(tokenRequestData);
-      });
-  }
 
-  function refreshToken(refreshToken) {
-    return tokenRequest({
-      refresh_token: refreshToken,
-      grant_type: "refresh_token",
-      redirect_uri: config.redirectUri
-    });
-  }
+        if (opts.scope) {
+            urlParams.scope = opts.scope;
+        }
 
-  return {
-    getAuthorizationCode: getAuthorizationCode,
-    getAccessToken: getAccessToken,
-    refreshToken: refreshToken
-  };
+        if (opts.accessType) {
+            urlParams.access_type = opts.accessType;
+        }
+
+        var url = config.authorizationUrl + "?" + queryString.stringify(urlParams);
+
+        const authWindow = new BrowserWindow(
+            windowParams || { "use-content-size": true }
+        );
+
+        authWindow.loadURL(url);
+        authWindow.show();
+
+        authWindow.on("closed", function() {
+            callback(new Error("window was closed by user"));
+        });
+
+        function onCallback(url) {
+            var url_parts = nodeUrl.parse(url, true);
+            var query = url_parts.query;
+            var code = query.code;
+            var error = query.error;
+
+            if (error !== undefined) {
+                callback(error);
+                authWindow.removeAllListeners("closed");
+                setImmediate(function() {
+                    setTimeout(function() {
+                        authWindow.webContents.session.clearStorageData({
+                                storages: ["appcache", "cookies", "filesystem", "shadercache"],
+                                quotas: ["persistent", "syncable"]
+                            },
+                            function() {
+                                authWindow.close();
+                                authWindow.destroy();
+                            }
+                        );
+                    }, 100);
+                });
+            } else if (code) {
+                callback(code);
+                authWindow.removeAllListeners("closed");
+                setImmediate(function() {
+                    setTimeout(function() {
+                        authWindow.webContents.session.clearStorageData({
+                                storages: ["appcache", "cookies", "filesystem", "shadercache"],
+                                quotas: ["persistent", "syncable"]
+                            },
+                            function() {
+                                authWindow.close();
+                                authWindow.destroy();
+                            }
+                        );
+                    }, 100);
+                });
+            }
+        }
+
+        authWindow.webContents.on("will-navigate", function(event, url) {
+            onCallback(url);
+        });
+        var filter = {
+            urls: [config.redirectUri + "*"]
+        };
+        authWindow.webContents.session.webRequest.onBeforeRequest(
+            filter,
+            (details, c) => {
+                var url = details.url;
+                if (url.startsWith(config.redirectUri)) {
+                    onCallback(url);
+                }
+                c({});
+            }
+        );
+        authWindow.webContents.on("did-get-redirect-request", function(
+            event,
+            oldUrl,
+            newUrl
+        ) {
+            onCallback(newUrl);
+        });
+    }
+
+    function tokenRequest(data, callback) {
+        const header = {
+            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
+        };
+
+        if (config.useBasicAuthorizationHeader) {
+            header.Authorization =
+                "Basic " +
+                new Buffer(config.clientId + ":" + config.clientSecret).toString(
+                    "base64"
+                );
+        } else {
+            objectAssign(data, {
+                client_id: config.clientId,
+                client_secret: config.clientSecret
+            });
+        }
+
+        fetch(config.tokenUrl, {
+            method: "POST",
+            headers: header,
+            body: queryString.stringify(data)
+        }).then(response => response.json()).then(function(data) {
+
+            callback(data);
+            // return res.json();
+        });
+    }
+
+    function getAccessToken(opts, callback) {
+        getAuthorizationCode(opts, function(authorizationCode) {
+            var tokenRequestData = {
+                code: authorizationCode,
+                grant_type: "authorization_code",
+                redirect_uri: config.redirectUri
+            };
+            tokenRequestData = Object.assign(
+                tokenRequestData,
+                opts.additionalTokenRequestData
+            );
+            tokenRequest(tokenRequestData, function(resp) {
+                callback(resp);
+            });
+        });
+    }
+
+    function refreshToken(refreshToken, callback) {
+        tokenRequest({
+                refresh_token: refreshToken,
+                grant_type: "refresh_token",
+                redirect_uri: config.redirectUri
+            },
+            function(resp) {
+                callback(resp);
+            }
+        );
+    }
+
+    return {
+        getAuthorizationCode: getAuthorizationCode,
+        getAccessToken: getAccessToken,
+        refreshToken: refreshToken
+    };
 };
